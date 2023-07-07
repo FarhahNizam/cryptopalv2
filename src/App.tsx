1ba7532de55edd21aa0c6e190c0617722a7ae40b
joeeeee7 <johanansamuel76@gmail.com>
2023-07-07 09:27:44 +0800

    push july 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,32 @@
-import * as React from 'react';
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
-import SignUp from '../src/Components/SignUp';
-import Home from '../src/Pages/Home'
-import Help from '../src/Pages/Help';
-import AboutUs from '../src/Pages/AboutUs';
-import NavBar from '../src/Components/Navigationbar';
-import SignIn from '../src/Components/SignIn';
-import Consolepage from '../src/Pages/Consolepage';
-import { Container } from 'react-bootstrap';
-import RealTimeChart from '../src/Components/Realtimechart';
-import Features from '../src/Pages/Features';
+import * as React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "../src/Components/SignUp";
+import Home from "../src/Pages/Home";
+import Help from "../src/Pages/Help";
+import AboutUs from "../src/Pages/AboutUs";
+import NavBar from "../src/Components/Navigationbar";
+import SignIn from "../src/Components/SignIn";
+import Consolepage from "../src/Pages/Consolepage";
+import { Container } from "react-bootstrap";
+import RealTimeChart from "../src/Components/Realtimechart";
+import Features from "../src/Pages/Features";
 
-
-const App: React.FC =() => {
-
+const App: React.FC = () => {
   return (
     <>
-
-    {/* <Routes>
+      {/* <Routes>  
           <Route path="/" element={<Consolepage />} />
-    </Routes>
+    </Routes> 
      */}
-     {/* <NavBar />  */}
-    <Routes>
-     <Route path="/" element={<Home />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/help" element={<Help />} />
-          <Route path="/features" element={<Features />} />
-
-    </Routes>
-
-  
-  </>
-   
+      {/* <NavBar />  */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/features" element={<Features />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from '../src/App';
-import reportWebVitals from '../src/reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import CryptoContext from '../src/CryptoContext';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import 'chart.js';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "../src/App";
+import reportWebVitals from "../src/reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import CryptoContext from "../src/CryptoContext";
+import "react-alice-carousel/lib/alice-carousel.css";
+import "chart.js";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-     <CryptoContext>
-    <BrowserRouter> 
-   
-    <App />
-  
-    </BrowserRouter>
+    <CryptoContext>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </CryptoContext>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
